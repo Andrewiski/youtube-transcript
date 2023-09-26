931a13dfb7ca86d51e01313f11107a6bfd47ed29
Andrewiski <adevries@digitalexample.com>
2023-09-26 15:50:53 -0400

    add visual code debug

--- example/cli.js
+++ example/cli.js
@@ -1,4 +1,8 @@
-const { YoutubeTranscript } = require('youtube-transcript');
+
+//const { YoutubeTranscript } = require('youtube-transcript');
+const { YoutubeTranscript } = require('../dist/youtube-transcript.common.js');
+
+
 YoutubeTranscript.fetchTranscript(process.argv[2])
   .then(console.log)
   .catch(console.error);
